cf96972055b7979c13fba7c6f7d502722423aeb1
JnBanks <joku.banks@gmail.com>
2022-04-26 20:48:16 -0700

    wrote the functions in index.js for adding team members

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
+//const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -7,46 +7,124 @@ const Intern = require('./lib/Intern');
 const employees = [];
 
 
-function askQuestion() {
+function buildTeam() {
     inquirer.prompt([
         {
-            name: 'question',
+            name: 'buildTeamPrompt',
             type: 'list',
-            choices: ['Add a Manager', 'Add an Engineer', 'Add an Intern', 'quit']
+            message: 'Who would you like to add to your team?',
+            choices: ['Manager', 'Engineer', 'Intern', 'Everyone has been added!']
         }
     ]) .then(answers => {
         switch (answers.question) {
-            case 'Add a Manager':
+            case 'Manager':
                 console.log('add manager!')
                 addManager();
                 break;
 
-            case 'Add an Engineer':
+            case 'Engineer':
                 console.log('add engineer!')
                 addEngineer();
                 break;
 
-            case 'Add an Intern':
+            case 'Intern':
                 console.log('add intern!')
                 addIntern();
                 break;
 
             default:
-                console.log('nice team building!')
+                htmlBuilder()
                 break;
         }
     })
 }
 
 function addManager() {
-    inquirer.prompt({
-        name: 'name',
-        message: "What is the manager's name?",
-        type: 'input'
-    }).then(({ name }) => {
-        console.log(name);
-        const newManager = new Manager(name);
-        employees.push(newManager)
-        console.log(employees)
+    inquirer.prompt([
+        {
+            name: 'managerName',
+            message: "What is the manager's name?",
+            type: 'input'
+        },
+        {
+            name: 'managerId',
+            message: "What is the manager's id?",
+            type: 'input'
+        },
+        {
+            name: 'managerEmail',
+            message: "What is the manager's email address?",
+            type: 'input'
+        },
+        {
+            name: 'managerOfficeNumber',
+            message: "What is the manager's office number?",
+            type: 'input'
+        }
+
+    ]).then(answers => {
+        const newManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        employees.push(newManager);
+        buildTeam();
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            name: 'engineerName',
+            message: "What is the engineer's name?",
+            type: 'input'
+        },
+        {
+            name: 'engineerId',
+            message: "What is the engineer's id?",
+            type: 'input'
+        },
+        {
+            name: 'engineerEmail',
+            message: "What is the engineer's email address?",
+            type: 'input'
+        },
+        {
+            name: 'engineerGithub',
+            message: "What is the engineer's Github?",
+            type: 'input'
+        }
+
+    ]).then(answers => {
+        const newEngineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        employees.push(newEngineer);
+        buildTeam();
+    })
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            name: 'internName',
+            message: "What is the intern's name?",
+            type: 'input'
+        },
+        {
+            name: 'internId',
+            message: "What is the intern's id?",
+            type: 'input'
+        },
+        {
+            name: 'internEmail',
+            message: "What is the intern's email address?",
+            type: 'input'
+        },
+        {
+            name: 'internSchool',
+            message: "What is the intern's school?",
+            type: 'input'
+        }
+
+    ]).then(answers => {
+        const newIntern = new Intern(answers.InternName, answers.InternId, answers.InternEmail, answers.InternSchool);
+        employees.push(newIntern);
+        buildTeam();
     })
 }
